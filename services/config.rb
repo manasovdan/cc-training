7f8a9a2649d37186e4d9e186ca4a44d16f8071ec
manasovdan <afroprogger@gmail.com>
2016-12-16 18:24:51 +0600

    test

--- services/config.rb
+++ services/config.rb
@@ -4,9 +4,9 @@ coreo_aws_advisor_alert 'daniel' do
   description 'Finding Unused Credentials'
   level 'Informational'
   objectives ['users']
-  audit_objects ['users.user_name']
-  operators ['=~']
-  alert_when [/coreo-\w*-\w*/]
+  audit_objects ['users.password_last_used.nil?']
+  operators ['==']
+  alert_when [true]
 end
 
 coreo_aws_advisor_iam 'resulting one' do
