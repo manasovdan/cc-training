4ad0d5f86a13a890247d07c3d8d958e3233e5c35
manasovdan <afroprogger@gmail.com>
2016-12-16 16:25:30 +0600

    added objective

--- services/config.rb
+++ services/config.rb
@@ -3,7 +3,7 @@ coreo_aws_advisor_alert 'daniel' do
   service :ec2
   description 'snapshots that are greater than 256 GB that are not encrypted'
   level 'Informational'
-  objectives ['describe_instances']
+  objectives ['describe_instances', 'describe_instances']
   audit_objects ['spot_instance_request_set.launch_specification.block_device_mapping.ebs.volume_size', 'spot_instance_request_set.launch_specification.block_device_mapping.ebs.ecrypted']
   operators ['>', '==']
   alert_when [8, false]
