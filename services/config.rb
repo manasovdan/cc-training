fb4c8ccc458fd02384bdf88e603074ead416fb5d
manasovdan <afroprogger@gmail.com>
2016-12-16 16:19:42 +0600

    changed storage size to 8GB max

--- services/config.rb
+++ services/config.rb
@@ -6,5 +6,5 @@ coreo_aws_advisor_alert 'daniel' do
   objectives ['describe_instances']
   audit_objects ['spot_instance_request_set.launch_specification.block_device_mapping.ebs.volume_size', 'spot_instance_request_set.launch_specification.block_device_mapping.ebs.ecrypted']
   operators ['>', '=']
-  alert_when [256, false]
+  alert_when [8, false]
 end
