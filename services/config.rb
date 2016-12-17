19d5a2ad54851693b04e998494c7e8bf196ebc53
manasovdan <afroprogger@gmail.com>
2016-12-17 10:48:20 +0600

    added alert to get unused keys

--- services/config.rb
+++ services/config.rb
@@ -1,15 +1,26 @@
-coreo_aws_advisor_alert 'daniel' do
+coreo_aws_advisor_alert 'iam-unused-passwords' do
   action :define
   service :iam
-  description 'Finding Unused Credentials'
+  description 'Finding Unused passwords'
   level 'Informational'
   objectives ['users']
-  audit_objects ['users.user_name']
+  audit_objects ['users.user_id']
+  operators ['=~']
+  alert_when [//]
+end
+
+coreo_aws_advisor_alert 'iam-unused-access-keys' do
+  action :define
+  service :iam
+  description 'Finding unused access keys'
+  level 'Informational'
+  objectives ['access_keys']
+  audit_objects ['access_key_metadata.access_key_id']
   operators ['=~']
   alert_when [//]
 end
 
 coreo_aws_advisor_iam 'resulting one' do
   action :advise
-  alerts [ 'daniel']
+  alerts [ 'iam-unused-access-keys']
 end
