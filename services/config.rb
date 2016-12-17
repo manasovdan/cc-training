b76b9d2a36fab059d540df938108f303ed394f83
manasovdan <afroprogger@gmail.com>
2016-12-17 10:24:13 +0600

    regexp for getting JSON structure

--- services/config.rb
+++ services/config.rb
@@ -4,9 +4,9 @@ coreo_aws_advisor_alert 'daniel' do
   description 'Finding Unused Credentials'
   level 'Informational'
   objectives ['users']
-  audit_objects ['']
-  operators ['']
-  alert_when ['']
+  audit_objects ['users.user_name']
+  operators ['=~']
+  alert_when [/coreo-\w*-\w*/]
 end
 
 coreo_aws_advisor_iam 'resulting one' do
